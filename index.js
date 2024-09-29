a2fc8a61ed91f55336a2a86dffafb62dde34c965
SajanDuraiswamy <151189677+SajanDuraiswamy@users.noreply.github.com>
2024-09-29 20:04:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const API_KEY = "71c4ac9a2ae4432e85b4aca634844082";
+const recipeListEl = document.getElementById("recipe-list");
+function displayRecipes(recipes){
+    recipeListEl.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
+        recipeImageEl = document.createElement("img");
+        recipeImageEl.src = recipe.image;
+        recipeImageEl.alt = "recipe image";
+        recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText = recipe.title;
+        recipeIngredientsEl = document.createElement("p");
+        recipeIngredientsEl.innerHTML = `<strong>Ingredients:</strong> ${recipe.extendedIngredients.map((ingredient) => ingredient.original).join(", ")}`;
+
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipeItemEl.appendChild(recipeIngredientsEl);
+        recipeListEl.appendChild(recipeItemEl);
+        
+        
+    });
+}
+async function getRecipes(){
+    const response =  await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`);
+    const data = await response.json();
+    return data.recipes;
+}
+async function init(){
+    const recipes = await getRecipes();
+    displayRecipes(recipes);
+}
+init();
